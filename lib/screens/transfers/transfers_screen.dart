b129b7b57e1048dac51a062c22e25d745857530a
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2025-02-26 03:52:41 +0200

    fix exception in transfer screen

--- lib/screens/transfers/transfers_screen.dart
+++ lib/screens/transfers/transfers_screen.dart
@@ -1,40 +1,34 @@
+import 'package:elkaweer/provider/transfer_provider.dart';
+import 'package:elkaweer/resources/strings_manager.dart';
 import 'package:elkaweer/screens/transfers/transfer_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'package:elkaweer/models/transfer_model.dart';
-import 'package:elkaweer/provider/transfer_provider.dart';
-import 'package:elkaweer/resources/strings_manager.dart';
 
 class TransfersScreen extends StatelessWidget {
   const TransfersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final transfersProvider = Provider.of<TransfersProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(AppStrings.transfers),
         centerTitle: true,
       ),
-      body: Consumer<TransfersProvider>(
-        builder: (context, provider, child) {
-          if (provider.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          return ListView.builder(
-            padding: const EdgeInsets.all(10),
-            itemCount: provider.transfers.length,
-            itemBuilder: (context, index) {
-              final TransferData transfer = provider.transfers[index];
-              return TransferCard(
-                  playerName: transfer.playerName,
-                  newClub: transfer.newClub,
-                  transferFee: transfer.transferFee);
-            },
-          );
-        },
-      ),
+      body: transfersProvider.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: transfersProvider.transfers.length,
+              itemBuilder: (context, index) {
+                final transfer = transfersProvider.transfers[index];
+                return TransferCard(
+                    playerName: transfer.playerName,
+                    newClub: transfer.newClub,
+                    transferFee: transfer.transferFee);
+              },
+            ),
     );
   }
 }
